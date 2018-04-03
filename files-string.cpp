a53ae98ebfc7c92f25f722a0bff2f63e3f9705fc
Carlos R. Arias <arias@spu.edu>
2018-04-03 10:13:56 -0700

    string-file.cpp tested

--- files-string.cpp
+++ files-string.cpp
@@ -21,13 +21,13 @@ int main(){
 	cin >> aString;
 	for (int i=0; i<aString.length(); i++){
 		if (isalpha(aString[i]))
-			cout << static_cast<int>(aString[i]) << aString[i] << "\t" << "Is a letter" << endl;
+			cout << static_cast<int>(aString[i]) << "\t" << aString[i] << "\t" << "Is a letter" << endl;
 		else if (isdigit(aString[i]))
-			cout << static_cast<int>(aString[i]) << aString[i] << "\t" << "Is a number" << endl;
+			cout << static_cast<int>(aString[i]) << "\t" << aString[i] << "\t" << "Is a number" << endl;
 		else if (ispunct(aString[i]))
-			cout << static_cast<int>(aString[i]) << aString[i] << "\t" << "Is a punctuation character" << endl;
+			cout << static_cast<int>(aString[i]) << "\t" << aString[i] << "\t" << "Is a punctuation character" << endl;
 		else
-			cout << static_cast<int>(aString[i]) << aString[i] << "\t" << "Is something else" << endl;
+			cout << static_cast<int>(aString[i]) << "\t" << aString[i] << "\t" << "Is something else" << endl;
 	}
 	ofstream myFile("string.txt");
 	if (myFile.fail()){
